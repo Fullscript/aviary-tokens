e308dfd8b12629386febbe32ed6a26ab22a44d86
iryanclarke <iryanclarke@users.noreply.github.com>
2023-01-23 19:04:13 +0000

    Transformed tokens

--- dist/types/themes.d.ts
+++
@@ -1,6 +0,0 @@
-import * as light from "../../dist/tokens/ts/themes/light";
-import * as dark from "../../dist/tokens/ts/themes/dark";
-declare type AviaryTheme = typeof light;
-export { dark, light };
-export type { AviaryTheme };
-//# sourceMappingURL=themes.d.ts.map

b802d1492251ba4a408dfb94fe081d8e47753d8c
iryanclarke <iryanclarke@users.noreply.github.com>
2023-05-23 17:02:26 +0000

    Built and updated design tokens

---
+++ dist/tokens/native/borders.js
@@ -0,0 +1,12 @@
+/**
+ * Do not edit directly
+ * Generated on Tue, 23 May 2023 17:02:25 GMT
+ */
+
+module.exports = {
+  "radiusBase": 8,
+  "radiusCircle": "50%",
+  "radiusModal": 8,
+  "radiusPill": 100,
+  "widthBase": 1
+};

857887e378dd55ffa2eee1ed61d1d1ee19f70039
laertejofrejr <laertejofrejr@users.noreply.github.com>
2023-07-25 18:29:19 +0000

    Built and updated design tokens

---
+++ dist/tokens/native/borders.d.ts
@@ -0,0 +1,10 @@
+/**
+ * Do not edit directly
+ * Generated on Tue, 25 Jul 2023 18:29:18 GMT
+ */
+
+export const radiusBase : number;
+export const radiusCircle : string;
+export const radiusModal : number;
+export const radiusPill : number;
+export const widthBase : number;

25af81ee11e08f2f7af54d85b6e7810b1a325b9b
iryanclarke <iryanclarke@users.noreply.github.com>
2023-08-15 12:36:38 +0000

    Built and updated design tokens

---
+++ dist/tokens/native/borders.d.ts
@@ -0,0 +1,10 @@
+/**
+ * Do not edit directly
+ * Generated on Tue, 15 Aug 2023 12:36:37 GMT
+ */
+
+export const radiusBase : number;
+export const radiusCircle : string;
+export const radiusModal : number;
+export const radiusPill : number;
+export const widthBase : number;

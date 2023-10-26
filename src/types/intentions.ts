93778f5de64c39d54436ef26b4d43321f11e8d30
Ian Clarke <ian.clarke@fullscript.com>
2023-10-26 14:44:16 -0400

    Better intentions

--- src/types/intentions.ts
+++ src/types/intentions.ts
@@ -1,8 +1,17 @@
-const AVIARY_INTENTIONS = {
+const CORE_AVIARY_INTENTIONS = {
   primary: "primary",
   system: "system",
   danger: "danger",
   success: "success",
+};
+
+const AVIARY_INTENTIONS = {
+  ...CORE_AVIARY_INTENTIONS,
+  warning: "warning",
+};
+
+const AVIARY_BUTTON_INTENTIONS = {
+  ...CORE_AVIARY_INTENTIONS,
   textSuccess: "textSuccess",
   textSystem: "textSystem",
   textDanger: "textDanger",
@@ -12,6 +21,7 @@ const AVIARY_INTENTIONS = {
 };
 
 type AviaryIntentions = keyof typeof AVIARY_INTENTIONS;
+type AviaryButtonIntentions = keyof typeof AVIARY_BUTTON_INTENTIONS;
 
-export type { AviaryIntentions };
-export { AVIARY_INTENTIONS };
+export type { AviaryButtonIntentions, AviaryIntentions };
+export { AVIARY_BUTTON_INTENTIONS, AVIARY_INTENTIONS };

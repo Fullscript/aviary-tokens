a94f16f7dbf1ff7f4e1f47feb769ef093974856b
brittany-fs <brittany-fs@users.noreply.github.com>
2024-01-29 20:04:39 +0000

    Transformed tokens

--- dist/tokens/ts/dimensions.d.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Do not edit directly
- * Generated on Wed, 24 Jan 2024 16:29:12 GMT
- */
-
-export const contentAreaMax : string;
--- dist/tokens/ts/dimensions.js
+++
@@ -1,8 +0,0 @@
-/**
- * Do not edit directly
- * Generated on Wed, 24 Jan 2024 16:29:12 GMT
- */
-
-module.exports = {
-  "contentAreaMax": "1196px"
-};

d2b97e826e2e15dec07f3cb93c95fadb19aa11f3
iryanclarke <iryanclarke@users.noreply.github.com>
2024-03-19 13:47:25 +0000

    Built and updated design tokens

---
+++ dist/tokens/ts/dimensions.d.ts
@@ -0,0 +1,6 @@
+/**
+ * Do not edit directly
+ * Generated on Tue, 19 Mar 2024 13:47:24 GMT
+ */
+
+export const contentAreaMax : string;
---
+++ dist/tokens/ts/dimensions.js
@@ -0,0 +1,8 @@
+/**
+ * Do not edit directly
+ * Generated on Tue, 19 Mar 2024 13:47:24 GMT
+ */
+
+module.exports = {
+  "contentAreaMax": "1196px"
+};

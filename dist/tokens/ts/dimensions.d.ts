685795efe294a8904b20a1d75279ab730183f8ec
iryanclarke <iryanclarke@users.noreply.github.com>
2024-03-19 13:26:03 +0000

    Transformed tokens

--- dist/tokens/ts/dimensions.d.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Do not edit directly
- * Generated on Mon, 18 Mar 2024 18:39:38 GMT
- */
-
-export const contentAreaMax : string;
--- dist/tokens/ts/dimensions.js
+++
@@ -1,8 +0,0 @@
-/**
- * Do not edit directly
- * Generated on Mon, 18 Mar 2024 18:39:38 GMT
- */
-
-module.exports = {
-  "contentAreaMax": "1196px"
-};

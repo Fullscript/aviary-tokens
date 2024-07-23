01109e60f1b3cb0526eb214cbd1e23a79f51c752
alexandra-lim <alexandra-lim@users.noreply.github.com>
2024-07-23 13:51:01 +0000

    Transformed tokens

--- dist/tokens/ts/dimensions.d.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Do not edit directly
- * Generated on Mon, 08 Jul 2024 12:56:30 GMT
- */
-
-export const contentAreaMax : string;
--- dist/tokens/ts/dimensions.js
+++
@@ -1,8 +0,0 @@
-/**
- * Do not edit directly
- * Generated on Mon, 08 Jul 2024 12:56:30 GMT
- */
-
-module.exports = {
-  "contentAreaMax": "1196px"
-};
